940a0849cad8b3f96c87eafb396eebf81ea30caa
Alicia Ramirez <ar@aliciaramirez.com>
2014-03-14 13:35:42 -0500

    Footer
    Added a sidebar to the footer in functions.php, and then added that
    sidebar in footer.php. Also, I added a custom loop in footer to display
    a random testimonial.

--- wp-content/themes/catorce/footer.php
+++ wp-content/themes/catorce/footer.php
@@ -1,6 +1,40 @@
 			<footer class="footer" role="contentinfo">
 			
 				<div id="inner-footer" class="row clearfix">
+
+					<div class="large-4 medium-12 columns">
+
+						<?php dynamic_sidebar('footer'); ?>
+
+					</div>
+
+					<div class="large-8 medium-12 columns">
+						
+						<?php 
+						// the query
+						$the_query = new WP_Query( 'post_type=testimonials&posts_per_page=1&orderby=rand' ); ?>
+						
+						<?php if ( $the_query->have_posts() ) : ?>
+						
+							<!-- pagination here -->
+						      
+							<!-- the loop -->
+							<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+							<blockquote><?php the_content(); ?></blockquote>
+							<p><?php the_field('client'); ?></p>
+							<p><a href="<?php echo home_url('testimonials'); ?>">More Testimonials</a></p>
+							<?php endwhile; ?>
+							<!-- end of the loop -->
+						      
+							<!-- pagination here -->
+						      
+							<?php wp_reset_postdata(); ?>
+						
+						<?php else:  ?>
+						  <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+						<?php endif; ?>
+
+					</div>
 				
 					<div class="large-12 medium-12 columns">
 						<nav role="navigation">
